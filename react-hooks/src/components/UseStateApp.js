c7c333eda0b2cd768385eeb67265b5d17381d58c
Marshood <marshood.ayoub@gmail.com>
2021-11-25 09:18:57 +0200

    adding useState

---
+++ react-hooks/src/components/UseStateApp.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+
+export default function UseStateApp() {
+const [count,seTcount] = useState(0);
+    return (
+        <div>
+            <h1>{count} </h1>
+            <button onClick={()=>{seTcount(count+1)}}>INC</button>
+            <button onClick={()=>{seTcount(count-1)}}>DEC</button>
+        </div>
+    )
+}
